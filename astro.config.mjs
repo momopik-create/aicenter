8a241b92de485d84ce247bae10e5a2df7af1fac0
momopik-create <momopik@protonmail.com>
2026-09-19 07:31:06 -0700

    Remove Tailwind integration from astro.config.mjs
    Removed Tailwind integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aicenter.pages.dev', // افزودن دامنه اصلی جهت رفع خطای sitemap
+  site: 'https://aicenter.pages.dev',
   integrations: [
-    tailwind(),
     sitemap(),
   ],
 });
