1b46237d357f28790ef67bbd14a6adb6577b12e4
momopik-create <momopik@protonmail.com>
2026-09-19 07:43:49 -0700

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://momopik.com', // آدرس سایت شما
+  site: 'https://aicenter-2vm.pages.dev', // آدرس سایت شما
   integrations: [
     sitemap(),
   ],
