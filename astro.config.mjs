9300893bbc6a7631e4a263520201c93f11afebc3
momopik-create <momopik@protonmail.com>
2026-09-19 01:20:25 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://aicenter.pages.dev', // بعداً با دامنه اصلی جایگزین می‌شود
+  integrations: [sitemap()],
+});
